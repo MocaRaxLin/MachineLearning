f843c38b2b9dcebe5931481b23c52e2ccb90a08f
Justin Lin <tony199462@gmail.com>
2016-01-17 12:07:44 +0800

    test file

---
+++ src/test.m
@@ -0,0 +1,9 @@
+clc
+clear
+
+
+%btr Center line 16
+% [centers, radii, metric] = imfindcircles(irisImage,[20 90]);
+% [r index] = max(radii)
+% centerPos = centers(index,:);
+% centerPos = fliplr(centerPos)
